f9b3db34e958a3ba73c7af0a8ed1ab3994f059ba
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 03:08:14 +0800

    starting the code, set variables also

--- quiz_runner/quiz_game.py
+++ quiz_runner/quiz_game.py
@@ -2,3 +2,9 @@ import random
 from inputtimeout import inputimeout, TimeoutOccured
 from colorama import Fore
 
+class Quizgame:
+    def __init__(self, difficulty, questions, time_limit = 15):
+        self.difficulty = difficulty
+        self.questions = questions
+        self.time_limit = time_limit
+        
