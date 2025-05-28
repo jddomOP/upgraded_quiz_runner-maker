55df7b30f93ac632e4da17139c7423b4228fa325
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 03:30:03 +0800

    it notifies user that a quiz file does not exist

--- quiz_runner/quiz_loader.py
+++ quiz_runner/quiz_loader.py
@@ -4,3 +4,6 @@ from colorama import Fore
 filename = 'quiz_questions_and_answers'
 
 def load_questions_by_difficulty(difficulty):
+    if not os.path.exists(filename):
+        print(FORE.RED + "QUIZ FILE NOT FOUND...")
+        return []
