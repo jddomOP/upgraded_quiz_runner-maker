83017c91212fa774e728b0b1a6209a1952cf697d
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 03:44:00 +0800

    adds option for user

--- quiz_runner/quiz_utilization.py
+++ quiz_runner/quiz_utilization.py
@@ -2,3 +2,6 @@ from quiz_loader import load_questions_by_difficulty
 from colorama import Fore
 
 def choose_difficulty():
+    difficulty_map = {'1': 'Elementary', '2': "High School", '3': 'General'}
+
+    while True
