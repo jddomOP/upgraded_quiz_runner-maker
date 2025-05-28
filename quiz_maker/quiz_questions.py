cfe1f71f75ca045d5362172f16b85894124477d0
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 02:25:29 +0800

    rename the class

--- quiz_maker/quiz_questions.py
+++ quiz_maker/quiz_questions.py
@@ -1,6 +1,6 @@
 #welcome, I'm still in the process of coding in OOP structure and this will serve as my practice also
 
-class quiz_questions:
+class quiz_question:
     def __init__(self, question, difficulty, choices, correct):
         self.questions = question
         self.difficulty = difficulty
