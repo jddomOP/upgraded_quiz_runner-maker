ba241c48c47c4844646986396137295615d48ecf
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 01:07:27 +0800

    added a parenthesis

--- quiz_maker/quiz_app.py
+++ quiz_maker/quiz_app.py
@@ -6,7 +6,7 @@ from quiz_questions import quiz_questions
 
 class quiz_runner:
     def __init__(self):
-        self.storage = quiz_storage
+        self.storage = quiz_storage()
 
     def display_title(self):
         print(pyfiglet.figlet_format("Welcome to the Quiz Maker!", font = 'slant'))
