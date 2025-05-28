594e84f49f15ef820cedd92183b11826419ebcee
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 03:57:55 +0800

    Displays a welcome message, prompts the user to choose difficulty and questions, initializes the quiz game, and starts the game.

--- quiz_runner/main_quiz_runner.py
+++ quiz_runner/main_quiz_runner.py
@@ -2,3 +2,14 @@ from quiz_game import QuizGame
 from utils import choose_difficulty
 import pyfiglet
 from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+def main():
+    print(Style.BRIGHT + Fore.LIGHTCYAN_EX + pyfiglet.figlet_format("Welcome to my Quiz!!", font="slant"))
+    difficulty, questions = choose_difficulty()
+    game = Quizgame(difficulty, questions)
+    game.start()
+
+if __name__ == "__main__":
+    main()
