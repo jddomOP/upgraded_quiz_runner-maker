ee75ceae39852ead805d8cb3974d363ff51a62dd
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 03:42:45 +0800

    add libraries, adds function for difficulty, adds this .py file for utilization of difficulty

--- quiz_runner/quiz_loader.py
+++ quiz_runner/quiz_loader.py
@@ -5,7 +5,7 @@ filename = 'quiz_questions_and_answers'
 
 def load_questions_by_difficulty(difficulty):
     if not os.path.exists(filename):
-        print(FORE.RED + "QUIZ FILE NOT FOUND...")
+        print(Fore.RED + "QUIZ FILE NOT FOUND...")
         return []
 
     with open(filename, 'r', encoding='utf-8') as file:
---
+++ quiz_runner/quiz_utilization.py
@@ -0,0 +1,4 @@
+from quiz_loader import load_questions_by_difficulty
+from colorama import Fore
+
+def choose_difficulty():
