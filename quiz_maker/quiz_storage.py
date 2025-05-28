e040fcef4e72899f1861a7aab978c00a02d71156
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 00:47:16 +0800

    define a function, adds a value for filename

--- quiz_maker/quiz_storage.py
+++ quiz_maker/quiz_storage.py
@@ -1,3 +1,9 @@
 #this will serve as a storer for questions and will serve for making .txt file
 
 import os
+
+class quiz_storage:
+    def __init__(self, filename = 'quiz_questions_and_answers'):
+        self.filename = filename
+
+    def save_question(self, quiz_data):
