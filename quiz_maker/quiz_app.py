8f0fad72f00cbdbfca0efabb3a103bb4ffcc426e
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 02:52:16 +0800

    couple of renamings

--- quiz_maker/quiz_app.py
+++ quiz_maker/quiz_app.py
@@ -1,8 +1,8 @@
 #here is the quiz app where we can make questions for the quiz
 
 import pyfiglet
-from quiz_storage import quiz_storage
-from quiz_questions import quiz_question
+from quiz_storage import Quizstorage
+from quiz_questions import Quizquestion
 
 class Quizrunner:
     def __init__(self):
