9e661a1cffbf9aa5e7716bcfd419371e9712c244
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 01:06:32 +0800

    Added quiz_runner class to manage quiz execution using quiz_storage. Added a function where title is displayed by using the pyfiglet to edit the text and add some sort of "style"

--- quiz_maker/quiz_app.py
+++ quiz_maker/quiz_app.py
@@ -4,3 +4,12 @@ import pyfiglet
 from quiz_storage import quiz_storage
 from quiz_questions import quiz_questions
 
+class quiz_runner:
+    def __init__(self):
+        self.storage = quiz_storage
+
+    def display_title(self):
+        print(pyfiglet.figlet_format("Welcome to the Quiz Maker!", font = 'slant'))
+
+
+
