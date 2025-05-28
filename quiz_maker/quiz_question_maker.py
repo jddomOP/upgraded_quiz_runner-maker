93507cbf4288eaadc473ff81260169893d8b586d
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 00:24:18 +0800

    defined question, difficulty, choices and correct answer

--- quiz_maker/quiz_question_maker.py
+++ quiz_maker/quiz_question_maker.py
@@ -1,5 +1,11 @@
 #welcome, I'm still in the process of coding in OOP structure and this will serve as my practice also
 
 class quiz_questions:
-    
+    def __init__(self, question, difficulty, choices, correct):
+        self.questions = question
+        self.difficulty = difficulty
+        self.choices = choices
+        self.correct = correct
+
+        
 
