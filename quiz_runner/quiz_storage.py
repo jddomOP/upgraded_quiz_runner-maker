d1c82a6445feb9228fa81f584e6ca8191766a23c
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 22:12:44 +0800

    well the 'filename' variable was not in the parenthesis so i adjusted it

--- quiz_runner/quiz_storage.py
+++ quiz_runner/quiz_storage.py
@@ -3,8 +3,8 @@
 import os
 
 class QuizStorage:
-    def __init__(self), filename = 'quiz_questions_and_answers':
-    self.filename = filename
+    def __init__(self, filename = 'quiz_questions_and_answers'):
+        self.filename = filename
 
     def load_questions_by_difficulty(self, difficulty):
         if not os.path.exists(self.filename):
