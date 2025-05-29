7cc23f6cbcab7cf58cfaabea6a094210de2454ea
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 22:24:30 +0800

    adds class and sets value to the ff. variable

--- quiz_runner/quiz_runner.py
+++ quiz_runner/quiz_runner.py
@@ -5,3 +5,10 @@ from colorama import init, Fore, Style
 from quiz_storage import QuizStorage
 
 init(autoreset=True)
+
+class QuizRunner:
+    def __init__(self, storage, time_limit = 15):
+        self.storage = storage
+        self.time_limit = time_limit
+        self.difficulty = None
+        self.questions = []
