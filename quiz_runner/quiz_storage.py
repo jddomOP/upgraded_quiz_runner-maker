c2daca13c961d3903dd04170cc8b4a6c0ce594d1
Jaydie <johndenieldomalaon@gmail.com>
2025-05-29 21:45:04 +0800

    if signal file is not found

--- quiz_runner/quiz_storage.py
+++ quiz_runner/quiz_storage.py
@@ -5,3 +5,7 @@ import os
 class QuizStorage:
     def __init__(self), filename = 'quiz_questions_and_answers':
     self.filename = filename
+
+    def load_questions_by_difficulty(self, difficulty):
+        if not os.path.exists(self.filename):
+            return None
